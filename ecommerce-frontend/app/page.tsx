34c95c2405d0c0927cfc094bc63ea8b03bd44635
Francisco Roldán <francisco.roldan@crombie.dev>
2023-10-04 18:19:48 -0300

    first commit

---
+++ ecommerce-frontend/app/page.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import products from "../products.json"
+// import glo
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Head>
+
+//       </Head>
+// {
+//   const cla = ´${}´
+// }
+
+//       {Object(products).map((product:Product) =>{
+//         <div key={product.attributes.name} className={styles.product}>
+//           // <div className={styles.product__Row}>
+//           //   <div className={styles.product__ColImg}>
+//           //     <img src=""/>
+//           //   </div>
+//           //   <div className="{styles.product__Col}">
+//               // {product.attributes.name} {product.attributes.price}
+//           //   </div>
+//           // </div>
+//         </div>
+//       })}
+//     </div>
+
+//   )
+// }
---
+++ ecommerce-frontend/types.d.ts
@@ -0,0 +1,16 @@
+type Product = {
+    id: number
+    attributes: Atributes
+}
+
+type Atributes = {
+    name: string,
+    content: string,
+    meta_decription: string,
+    meta_title: string,
+    price: number,
+    slug: string,
+    createdAt: string,
+    updatedAt: string,
+    publishedAt: string
+}
